19b33408d16e6e8d2fbc40989deb956649752096
Chris Choi <chrisbeenchoi@icloud.com>
2022-10-31 12:35:42 -0700

    beginning

---
+++ Life180/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Life180
+//
+//  Created by Chris Choi on 10/31/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
