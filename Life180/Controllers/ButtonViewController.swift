6b3a0b0a369e0dd153126070f33399414317ded9
Chris Choi <chrisbeenchoi@icloud.com>
2022-10-31 14:42:31 -0700

    beginning 2

--- Life180/Controllers/ButtonViewController.swift
+++ Life180/Controllers/ButtonViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ButtonViewController.swift
 //  Life180
 //
 //  Created by Chris Choi on 10/31/22.
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ButtonViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
